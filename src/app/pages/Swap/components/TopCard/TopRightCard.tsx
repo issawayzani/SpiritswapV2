0a35ef98c6a9fcd595586667a3ddbf73bc8955ea
issawayzani <d799888@gmail.com>
2023-06-30 01:21:54 +0300

    connecting write calls

--- src/app/pages/Swap/components/TopCard/TopRightCard.tsx
+++ src/app/pages/Swap/components/TopCard/TopRightCard.tsx
@@ -2,19 +2,30 @@ import { Box, Button, Flex, Spacer } from '@chakra-ui/react';
 import { SwapContainer } from '../../styles';
 import './styles.css';
 import { StatisticsPanel } from '../Panels';
+import { getReward } from 'utils/web3/actions/inspirit';
+import { transactionResponse } from 'utils/web3';
+import Web3Monitoring from 'app/connectors/EthersConnector/transactions';
+import { useState } from 'react';
+
 export default function TopRightCard(props) {
-  const powerPrice =
+  const { addToQueue } = Web3Monitoring();
+  const [isLoadingButton, setIsLoadingButton] = useState(false);
+  const [loaderText, setLoaderText] = useState('Loading Transaction');
+  const OTOKENPrice =
     (props.bondingCurveData?.priceOTOKEN *
       props.bondingCurveData?.accountOTOKEN) /
     1e18;
-  const glovePrice =
+  const OTOKENRewards = props.bondingCurveData?.accountEarnedOTOKEN / 1e18;
+  const TOKENPrice =
     (props.bondingCurveData?.priceTOKEN *
       props.bondingCurveData?.accountTOKEN) /
     1e18;
-  const OPPrice =
+  const TOKENRewards = props.bondingCurveData?.accountEarnedTOKEN / 1e18;
+  const BASEPrice =
     (props.bondingCurveData?.priceBASE * props.bondingCurveData?.accountBASE) /
     1e18;
-  const xGLOVEPrice =
+  const BASERewards = props.bondingCurveData?.accountEarnedBASE / 1e18;
+  const VTOKENPrice =
     (props.bondingCurveData?.priceTOKEN *
       props.bondingCurveData?.accountVTOKEN) /
     1e18;
@@ -27,38 +38,58 @@ export default function TopRightCard(props) {
       props.bondingCurveData?.accountBorrowCredit) /
     1e18;
   const buttonAction = async () => {
-    //need to get the rewards here
+    setIsLoadingButton(true);
+    // if (OTOKENRewards===0 && TOKENRewards===0 && BASERewards===0 ){
+
+    // }else {
+    try {
+      const tx = await getReward(props?.account);
+      const response = transactionResponse('swap.claim', {
+        operation: 'SWAP',
+        tx: tx,
+        uniqueMessage: {
+          text: 'Claiming',
+          secondText: 'Rewards',
+        },
+      });
+
+      addToQueue(response);
+      await tx.wait();
+      setIsLoadingButton(false);
+    } catch (e) {
+      setIsLoadingButton(false);
+    }
   };
   return (
     <Box mb="10px">
       <SwapContainer>
         <StatisticsPanel
           name="oSOUL"
-          rewards={props.bondingCurveData?.accountEarnedOTOKEN / 1e18}
+          rewards={OTOKENRewards}
           value={props.bondingCurveData?.accountOTOKEN / 1e18}
-          valueUSD={powerPrice}
+          valueUSD={OTOKENPrice}
           check={true}
         />
 
         <StatisticsPanel
           name="SOUL"
-          rewards={props.bondingCurveData?.accountEarnedTOKEN / 1e18}
+          rewards={TOKENRewards}
           value={props.bondingCurveData?.accountTOKEN / 1e18}
-          valueUSD={glovePrice}
+          valueUSD={TOKENPrice}
           check={true}
         />
         <StatisticsPanel
           name="WFTM"
-          rewards={props.bondingCurveData?.accountEarnedBASE / 1e18}
+          rewards={BASERewards}
           value={props.bondingCurveData?.accountBASE / 1e18}
-          valueUSD={OPPrice}
+          valueUSD={BASEPrice}
           check={true}
         />
 
         <StatisticsPanel
           name="vSOUL"
           value={props.bondingCurveData?.accountVTOKEN / 1e18}
-          valueUSD={xGLOVEPrice}
+          valueUSD={VTOKENPrice}
           check={false}
         />
 
@@ -76,8 +107,15 @@ export default function TopRightCard(props) {
           check={false}
         />
 
-        <Button size="lg" mt="16px" w="full" onClick={buttonAction}>
-          Harvest
+        <Button
+          size="lg"
+          mt="16px"
+          w="full"
+          onClick={buttonAction}
+          isLoading={isLoadingButton}
+          loadingText={loaderText}
+        >
+          Claim
         </Button>
       </SwapContainer>
     </Box>
