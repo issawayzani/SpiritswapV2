41b4f9e7c80eb499cbca525edb18cb30905fd653
issawayzani <d799888@gmail.com>
2023-07-24 23:05:19 +0300

    change values and add connect wallet changes

--- src/app/pages/Home/components/Panels/StatisticsPanel.tsx
+++ src/app/pages/Home/components/Panels/StatisticsPanel.tsx
@@ -9,10 +9,13 @@ export default function StatisticsPanel(props) {
         <Spacer />
         <div className="right-section">
           <span className="item-count">
-            {props.valueUSD} {props.name}
+            {props.check ? 'Connect Wallet' : props.value}{' '}
+            {props.check ? '' : props.name}
           </span>
           <br />
-          <span className="item-price">Balance: ${props.value}</span>
+          <span className="item-price">
+            Balance: ${props.check ? '0.0' : props.valueUSD}
+          </span>
         </div>
       </Flex>
     </Box>
